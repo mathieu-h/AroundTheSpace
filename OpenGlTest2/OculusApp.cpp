23adba55cf21c538b6e7779653dd428c60055a56
Maxime Lahaye <maxlahaye@me.com>
2015-05-01 17:01:16 +0200

    Oculus SDK. GLM library.

---
+++ OpenGlTest2/OculusApp.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "OculusApp.h"
+
+
+OculusApp::OculusApp()
+{
+}
+
+
+OculusApp::~OculusApp()
+{
+}
---
+++ OpenGlTest2/OculusApp.h
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include "OculusManagerApp.h"
+
+class OculusApp : public OculusManagerApp
+{
+public:
+	OculusApp();
+	~OculusApp();
+};
+
